1700cb2f9c4e9bcbf31555cb20e83ac6ca1b8b44
BboyMagik <jie22shen@gmail.com>
2021-10-15 13:14:13 -0400

    Add files via upload

---
+++ removebg.py
@@ -0,0 +1,16 @@
+# Auto detect text files and perform LF normalization
+* text=auto
+
+import requests
+
+response = requests.post(
+    'https://api.remove.bg/v1.0/removebg',
+    files={'image_file': open('/path/to/file.jpg', 'rb')},
+    data={'size': 'auto'},
+    headers={'X-Api-Key': 'x2KWpxDUELGMazkrF3WB4bdX'},
+)
+if response.status_code == requests.codes.ok:
+    with open('no-bg.png', 'wb') as out:
+        out.write(response.content)
+else:
+    print("Error:", response.status_code, response.text)
